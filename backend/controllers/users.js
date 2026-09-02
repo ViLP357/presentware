624e2bf8fe7a04361f7a7c1354a2f4751777c2f2
ViLP357 <vilipi2009@gmail.com>
2026-09-02 20:36:12 +0300

    add put and delete requests for user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -7,11 +7,20 @@ usersRouter.get('/', (request, response) => {
     response.json(users)
   })
 })
+usersRouter.get('/:id', (request, response, next)=> {
+    User.findById(request.params.id).then((user)=> {
+    response.json(user)
+  })
+  .catch(error=>next(error))
+})
 
+usersRouter.get('/:id', (request, response) => {
+  User.findByIdAndDelete(request.params.id)
+  .then(result=>{response.status(204).end()})
+})
 
 usersRouter.post('/', (request, response) => {
   const body = request.body
-
   if (!body.username ||!body.password) {
     return response.status(400).json({ 
       error: 'Username or password missing' 
@@ -24,6 +33,21 @@ usersRouter.post('/', (request, response) => {
   user.save().then((savedUser)=> {
     response.json(savedUser)
   })
+})
+usersRouter.put('/:id', (request, response,next)=> {
+  const {username} = request.body
+  User.findById(request.params.id)
+  .then(user=> {
+    if (!user) {
+      return response.status(404).end()
+    }
+    user.username = username
+
+    return user.save().then((updatedUser)=> {
+      response.json(updatedUser)
+    })
+  })
+  .catch(error=>next(error))
 })
 
 module.exports = usersRouter
